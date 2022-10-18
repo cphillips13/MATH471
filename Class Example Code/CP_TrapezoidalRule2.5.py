1770d6916515dc7fe30f43a9b0827eb27da8ffd7
Corey Phillips <cep.personal@protonmail.com>
2022-10-18 13:21:13 -0400

    Update CP_TrapezoidalRule2.5.py

--- Class Example Code/CP_TrapezoidalRule2.5.py
+++ Class Example Code/CP_TrapezoidalRule2.5.py
@@ -17,11 +17,12 @@ def Trap1(f, n, lowlim, uplim):
 
     h = (uplim - lowlim) / n
 
-    for i in range( 1, n):
+    for i  in range( 1, n):
         x = lowlim + i*h
         sum = sum + f(x)
     
     return(h*sum)
+
 
 #Determines error in number of subintervals used for
 #given theoretical bound on |f''(x)| (simple) Trapezoidal rule
@@ -36,7 +37,7 @@ def Trap1(f, n, lowlim, uplim):
 
 def ErrorTrap(lowlim, uplim, errtol, fdoubleprime_bound, verbose = False):
     #Arbitrary choice 100 here
-    MAX = 100
+    MAX = 1000
     for n in range(1, MAX):
         #Slower moves linearly through # of subintervals:
         h = (uplim - lowlim)/n
@@ -44,7 +45,7 @@ def ErrorTrap(lowlim, uplim, errtol, fdoubleprime_bound, verbose = False):
         #Faster - double the number of subintervals on each estimation:
         #h = (uplim - lowlim) / (2**n)
 
-        err = (uplim - lowlim) / 12.0 * (math.pow*(h,2)) * fdoubleprime_bound
+        err = ((uplim - lowlim) / 12.0) * ((math.pow*(h,2)) * (fdoubleprime_bound))
 
         if(err <= errtol):
             if(verbose):
@@ -63,3 +64,58 @@ T4 = Trap1(Ierf, 4, a, b)
 T8 = Trap1(Ierf, 8, a, b)
 T16 = Trap1(Ierf, 16, a, b)
 IR = (4.0*T8 - T4)/3.0
+
+#What does theory tell us?
+#   We have a bound: | I(f) - T_n(f)|
+#                       <= 1/12 * (b-a) *h^2 * max_[0,1] |f''(x)|
+
+#Ex 2.6 check trapezoidal rule
+checkval = .746824132812
+
+print('Integral approx. Ex. 2.6 (h = 1/4): {:8.8f} checkval: {:8.8f}'\
+    .format(T4, checkval))
+    
+print('Integral approx. Ex. 2.6 (h = 1/4): {:8.8f} checkval: {:8.8f}'\
+    .format(T8, checkval))
+    
+print('Integral approx. Ex. 2.6 (h = 1/4): {:8.8f} checkval: {:8.8f}'\
+    .format(T16, checkval))
+
+#Find minimum # of trapezoids to estimate integal within 10^-3 error:
+error_tolerance = math.pow(10.0, -3.0)
+
+#Establish |f''(x)| upper bound on [0,1] where
+#   f''(x) = e^(-x^2)*(4x^2 - 2), which can be checked to be < 1:
+fdpb = 1.0
+
+#Establish theoretical upper bound on number of trapezoids required:
+numtraps = ErrorTrap(a, b, error_tolerance, fdpb)
+
+print(('\nNumber of trapezoids theoeretically required to guarantee' + 'approximation within {} error tolerance: n = {}\n')\
+    .format(error_tolerance, numtraps))
+
+
+
+
+
+#Counter for number of trapezoids used in approximation:
+i = 0
+
+#inintialize errdiff > error_tolerance
+errdiff = error_tolerance + 1
+
+#Brute force check, using while loop:
+print('\nComparison of approximation to checkval usage:')
+while(errdiff >= error_tolerance):
+    #increment counter of number of trapezoids
+    i = i + 1
+
+    #Trapezoidal Approximation of integral with i trapezoids:
+    approximation_of_integral = Trap1(Ierf, i, a, b)
+    errdiff = abs(checkval - approximation_of_integral)
+
+    print('n={} approx: {:8.8f} checkval: {:8.8f} errdiff: {:8.8f}'\
+        .format(i, approximation_of_integral, checkval, errdiff))
+
+#Print final number of trapezoids actually required:
+print(('\nActual number of trapezoids required: {} , approx: {:8.8f},' + 'checckval: {:8.8f}'))
