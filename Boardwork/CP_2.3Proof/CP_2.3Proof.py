655bea9de295e8d469f05202663a1cbcf87cc480
Corey Phillips <cep.personal@protonmail.com>
2022-10-26 10:06:44 -0400

    Updates

--- Boardwork/CP_2.3Proof/CP_2.3Proof.py
+++ Boardwork/CP_2.3Proof/CP_2.3Proof.py
@@ -2,8 +2,12 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 
+import library
+import ch2lib.py as ch2
+
 x0 = 0
 x1 = 5
+
 
 x = np.linspace(x0, x1, 200)
 y = abs(x**2 - x*x1 - x*x0 + x0*x1)
@@ -14,4 +18,8 @@ ax.plot(x, y)
 plt.axvline(x0+x1/2, color = 'b', label = 'axvline - full height')
 
 plt.show()
+
+
+ch2.hello()
+
 
