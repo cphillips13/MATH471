25195dc60714081aab5ee6e66a6ce7df1c59cb32
Corey Phillips <cep.personal@protonmail.com>
2023-02-27 11:06:09 -0500

    updates

--- gui/untitled.py
+++ gui/untitled.py
@@ -4,18 +4,17 @@
 from remi.gui import *
 from remi import start, App
 
-
-class untitled(App):
+class field(App):
     def __init__(self, *args, **kwargs):
         if not 'editing_mode' in kwargs.keys():
-            super(untitled, self).__init__(*args, static_file_path={'my_res':'./res/'})
+            super(field, self).__init__(*args, static_file_path={'my_res':'./res/'})
 
     def idle(self):
         #idle function called every update cycle
         pass
     
     def main(self):
-        return untitled.construct_ui(self)
+        return field.construct_ui(self)
         
     @staticmethod
     def construct_ui(self):
@@ -512,11 +511,11 @@ class untitled(App):
 
 
 #Configuration
-configuration = {'config_project_name': 'untitled', 'config_address': '0.0.0.0', 'config_port': 8081, 'config_multiple_instance': True, 'config_enable_file_cache': True, 'config_start_browser': True, 'config_resourcepath': './res/'}
+configuration = {'config_project_name': 'field', 'config_address': '0.0.0.0', 'config_port': 8081, 'config_multiple_instance': True, 'config_enable_file_cache': True, 'config_start_browser': True, 'config_resourcepath': './res/'}
 
 if __name__ == "__main__":
     # start(MyApp,address='127.0.0.1', port=8081, multiple_instance=False,enable_file_cache=True, update_interval=0.1, start_browser=True)
-    start(untitled, address=configuration['config_address'], port=configuration['config_port'], 
+    start(field, address=configuration['config_address'], port=configuration['config_port'], 
                         multiple_instance=configuration['config_multiple_instance'], 
                         enable_file_cache=configuration['config_enable_file_cache'],
                         start_browser=configuration['config_start_browser'])
